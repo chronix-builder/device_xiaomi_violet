2c0a3a3a89582e0c656a540a5295058e76c999cc
Joker-V2 <nipinna0@gmail.com>
2025-09-09 16:26:53 +0200

    violet: Enable face unlock

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -11,6 +11,9 @@ $(call inherit-product, device/xiaomi/violet/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
 # Inherit MiuiCamera
 $(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
 
